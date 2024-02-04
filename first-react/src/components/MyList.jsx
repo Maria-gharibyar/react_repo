800c0e254d4ba25428719f29953d375628124173
Maria gharibyar <mariagharibyar@gmail.com>
2024-02-04 18:04:07 -0500

    First React

---
+++ first-react/src/components/MyList.jsx
@@ -0,0 +1,15 @@
+function MyList(){
+    return(
+       <>
+       <h1>Hello Dojo!</h1>
+       <h2>Things I need to do</h2>
+        <ul className="star">
+            <li>Learn React</li>
+            <li>Climb</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+        </ul>
+       </>
+    )
+}
+export default MyList 

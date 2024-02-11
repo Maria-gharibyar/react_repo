9e04ba6e1aebfe4fa33afded12bb747ed207c07a
Maria-Gharibyar <60002076+Maria-gharibyar@users.noreply.github.com>
2024-02-10 20:04:38 -0500

    Update Flip coins App.jsx

--- Practice/practice_1/promise/src/App.jsx
+++ Practice/practice_1/promise/src/App.jsx
@@ -5,47 +5,38 @@ import './App.css'
 import GetPokemon from './components/GetPokemon'
 
 function App() {
- 
-  // const [count, setCount] = useState(0)
-  //     const returnTrue= new Promise((resolve,reject)=>{
-  //           if((9+2)%2==0){
-  //             resolve(3+2);
-  //           }else{
-  //             reject("it is not odd number")
-  //           }
+ onst flip=()=>{return Math.random() > 0.5 ? "head":"tails"}
+  function fiveHeads() {
+    let head = 0;
+    let attempt = 0;
+    return new Promise( (resolve, reject) => {
 
-
-  //     })
-  //     returnTrue
-  //     .then(res=> console.log("the odd number is",res))
-  //     .catch(err=>console.log("there is error",err));
-
-  // function fiveHeads() {
-  //   let head = 0;
-  //   let attempt = 0;
-  //   return new Promise( (resolve, reject) => {
-
-  //       let flip=  Math.random() > 0.5 ? "head":"tails";
-  //       if(flip=="head"){
-  //         head++
-  //        }else{
-  //         head=0
-  //        }
-  //        attempt++
-  //        if(head === 5){
-  //         resolve(`It took ${attempt} attempts to get five heads`)
-  //        }else if(attempt === 100){
-  //         reject("you attempt more than 100")
-  //        }
+  
+        while( head < 5 && attempt<=100){ 
+          attempt++
+          let results=flip();
+          if(results == "head"){
+            head++
+           }else{
+            head=0
+           }
+        }
+       
+        
+         if(attempt <= 100){
+          resolve(`It took ${attempt} attempts to get five heads`)
+         }else{
+          reject(`Attempts have exceeded ${attempt} flips.`);
+         }
            
           
        
-//     });
-// }
-// fiveHeads()
-//     .then( res => console.log(res) )
-//     .catch( err => console.log(err) );
-// console.log( "When does this run now?" );
+    });
+}
+fiveHeads()
+    .then( res => console.log(res) )
+    .catch( err => console.log(err) );
+console.log( "When does this run now?" );
   return (
     <>
     <GetPokemon></GetPokemon>
